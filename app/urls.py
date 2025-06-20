9887b173c0e5ae098b1cebe263d35862900ed97e
Kim-Onesmus <kimones1122@gmail.com>
2025-06-20 09:43:14 +0300

    updates

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('register_user/', views.register_user, name='register'),
     path('user_login/', views.login_view, name='login'),
+    path('user_logout/', views.logout_view, name='logout'),
     path('refresh_token/', views.refresh_token_view, name='custom_token_refresh'),
 ] 
